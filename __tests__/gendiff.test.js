20f774b3256958ce35b4cd1d4deb27998c1ac574
Damir <damir.doszhan@gmail.com>
2020-05-12 08:59:36 +0600

    Completed 4th step of 2nd Project

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,19 +4,33 @@ import genDiff from '../src';
 
 describe('Testing JSON files', () => {
   test('Testing flat JSON files', () => {
-    const flatJSONPathBefore = `${__dirname}/__fixtures__/json/before.json`;
-    const flatJSONPathAfter = `${__dirname}/__fixtures__/json/after.json`;
-    const expected = fs.readFileSync(`${__dirname}/__fixtures__/json/result`, 'utf-8');
-    expect(genDiff(flatJSONPathBefore, flatJSONPathAfter)).toBe(expected.slice(0, -1));
+    const flatJSONPathBefore = `${__dirname}/__fixtures__/json/before-flat.json`;
+    const flatJSONPathAfter = `${__dirname}/__fixtures__/json/after-flat.json`;
+    const expectedFlatJson = fs.readFileSync(`${__dirname}/__fixtures__/json/result-flat`, 'utf-8');
+    expect(genDiff(flatJSONPathBefore, flatJSONPathAfter)).toBe(expectedFlatJson);
+  });
+
+  test('Testing deep JSON files', () => {
+    const deepJSONPathBefore = `${__dirname}/__fixtures__/json/before-deep.json`;
+    const deepJSONPathAfter = `${__dirname}/__fixtures__/json/after-deep.json`;
+    const expectedDeepJson = fs.readFileSync(`${__dirname}/__fixtures__/json/result-deep`, 'utf-8');
+    expect(genDiff(deepJSONPathBefore, deepJSONPathAfter)).toEqual(expectedDeepJson);
   });
 });
 
 describe('Testing YAML files', () => {
   test('Testing flat YAML files', () => {
-    const flatYamlPathBefore = `${__dirname}/__fixtures__/yaml/before.yaml`;
-    const flatYamlPathAfter = `${__dirname}/__fixtures__/yaml/after.yaml`;
+    const flatYamlPathBefore = `${__dirname}/__fixtures__/yaml/before-flat.yaml`;
+    const flatYamlPathAfter = `${__dirname}/__fixtures__/yaml/after-flat.yaml`;
     const expectedFlatYaml = fs.readFileSync(`${__dirname}/__fixtures__/yaml/result-flat`, 'utf-8');
-    expect(genDiff(flatYamlPathBefore, flatYamlPathAfter)).toEqual(expectedFlatYaml.slice(0, -1));
+    expect(genDiff(flatYamlPathBefore, flatYamlPathAfter)).toEqual(expectedFlatYaml);
+  });
+
+  test('Testing deep YAML files', () => {
+    const deepYamlPathBefore = `${__dirname}/__fixtures__/yaml/before-deep.yaml`;
+    const deepYamlPathAfter = `${__dirname}/__fixtures__/yaml/after-deep.yaml`;
+    const expectedDeepYaml = fs.readFileSync(`${__dirname}/__fixtures__/yaml/result-deep`, 'utf-8');
+    expect(genDiff(deepYamlPathBefore, deepYamlPathAfter)).toEqual(expectedDeepYaml);
   });
 });
 
@@ -25,6 +39,13 @@ describe('Testing INI files', () => {
     const flatIniPathBefore = `${__dirname}/__fixtures__/ini/before-flat.ini`;
     const flatIniPathAfter = `${__dirname}/__fixtures__/ini/after-flat.ini`;
     const expectedFlatIni = fs.readFileSync(`${__dirname}/__fixtures__/ini/result-flat`, 'utf-8');
-    expect(genDiff(flatIniPathBefore, flatIniPathAfter)).toEqual(expectedFlatIni.slice(0, -1));
+    expect(genDiff(flatIniPathBefore, flatIniPathAfter)).toEqual(expectedFlatIni);
+  });
+
+  test('Testing dee[] INI files', () => {
+    const deepIniPathBefore = `${__dirname}/__fixtures__/ini/before-flat.ini`;
+    const deepIniPathAfter = `${__dirname}/__fixtures__/ini/after-flat.ini`;
+    const expectedDeepIni = fs.readFileSync(`${__dirname}/__fixtures__/ini/result-flat`, 'utf-8');
+    expect(genDiff(deepIniPathBefore, deepIniPathAfter)).toEqual(expectedDeepIni);
   });
 });
--- src/index.js
+++ src/index.js
@@ -3,29 +3,90 @@ import path from 'path';
 import lodash from 'lodash';
 import generateObj from './parsers';
 
-const parse = (beforeObj, afterObj, keys) => keys.reduce((acc, key) => {
+const generateAst = (key, beforeValue, afterValue) => {
   const resultObj = {
     name: key,
-    value: afterObj[key],
-    sign: '\t  ',
+    type: '',
+    valueBefore: beforeValue,
+    valueAfter: afterValue,
+    children: null,
   };
 
-  if (lodash.has(beforeObj, key) && lodash.has(afterObj, key) && beforeObj[key] !== afterObj[key]) {
-    return [...acc, lodash.assign(resultObj, { sign: '\t+ ' }), lodash.assign({}, resultObj, { value: beforeObj[key], sign: '\t- ' })];
+  if (lodash.isObject(beforeValue) && lodash.isObject(afterValue)) {
+    const valKeys = lodash.union(Object.keys(beforeValue), Object.keys(afterValue));
+
+    return {
+      name: key,
+      type: 'object',
+      valueBefore: null,
+      afterValue: null,
+      children:
+        valKeys
+          .sort().map((valKey) => generateAst(valKey, beforeValue[valKey], afterValue[valKey])),
+    };
   }
 
-  if (!lodash.has(beforeObj, key) && lodash.has(afterObj, key)) {
-    return [...acc, lodash.assign(resultObj, { sign: '\t+ ' })];
+  if (beforeValue === undefined) {
+    return { ...resultObj, type: 'added', valueBefore: null };
   }
 
-  if (lodash.has(beforeObj, key) && !lodash.has(afterObj, key)) {
-    return [...acc, lodash.assign(resultObj, { sign: '\t- ', value: beforeObj[key] })];
+  if (afterValue === undefined) {
+    return { ...resultObj, type: 'removed', valueAfter: null };
   }
 
-  return [...acc, resultObj];
-}, []);
+  if (afterValue === beforeValue) {
+    return { ...resultObj, type: 'similar' };
+  }
 
-const render = (data) => data.map((item) => `${item.sign}${item.name}: ${item.value}`).join('\n');
+  return { ...resultObj, type: 'modified' };
+};
+
+const buildAst = (beforeObj, afterObj) => {
+  const keys = lodash.union(Object.keys(beforeObj), Object.keys(afterObj));
+
+  return keys.sort().map((key) => generateAst(key, beforeObj[key], afterObj[key]));
+};
+
+const parseValue = (value, level) => {
+  const space = '  ';
+  if (lodash.isObject(value)) {
+    const valueKeys = Object.keys(value);
+    const valuesCollection = valueKeys.map((key) => `${space.repeat(level + 2)}${key}: ${value[key]}`).join('\n');
+    return `{\n${valuesCollection}\n${space.repeat(level)}}`;
+  }
+
+  return value;
+};
+
+const buildLine = (obj, level) => {
+  const space = '  ';
+  const added = '+ ';
+  const removed = '- ';
+
+  const {
+    name,
+    type,
+    valueBefore,
+    valueAfter,
+    children,
+  } = obj;
+
+  switch (type) {
+    case 'object':
+      return `${space.repeat(level + 1)}${name}: {\n${children.map((child) => buildLine(child, level + 2)).join('\n')}\n${space.repeat(level + 1)}}`;
+    case 'added':
+      return `${space.repeat(level)}${added}${name}: ${parseValue(valueAfter, level + 1)}`;
+    case 'removed':
+      return `${space.repeat(level)}${removed}${name}: ${parseValue(valueBefore, level + 1)}`;
+    case 'modified':
+      return `${space.repeat(level)}${added}${name}: ${parseValue(valueAfter, level + 1)}\n${space.repeat(level)}${removed}${name}: ${parseValue(valueBefore, level + 1)}`;
+    default:
+      return `${space.repeat(level + 1)}${name}: ${parseValue(valueAfter, level + 1)}`;
+  }
+};
+
+const parseAst = (ast, level = 0) => ast
+  .map((obj) => buildLine(obj, level + 1)).join('\n');
 
 export default (beforeFilePath, afterFilePath) => {
   const beforeFile = fs.readFileSync(beforeFilePath, 'utf-8');
@@ -35,8 +96,8 @@ export default (beforeFilePath, afterFilePath) => {
 
   const beforeFileData = generateObj(beforeFileExt)(beforeFile);
   const afterFileData = generateObj(afterFileExt)(afterFile);
-  const allKeys = lodash.union(Object.keys(beforeFileData), Object.keys(afterFileData));
 
-  const parsedData = parse(beforeFileData, afterFileData, allKeys);
-  return `{\n${render(parsedData)}\n}`;
+  const ast = buildAst(beforeFileData, afterFileData);
+
+  return `{\n${parseAst(ast)}\n}`;
 };
